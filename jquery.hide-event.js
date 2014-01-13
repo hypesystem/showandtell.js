88100fb2e3e2410399f609c3c167ca02492251f3
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-01-13 18:17:54 +0100

    Added support for jQuery CSS function Monitoring display, visibility, and opacity.

--- jquery.hide-event.js
+++ jquery.hide-event.js
@@ -1,11 +1,11 @@
 /**
  * jquery.hide-event.js
- * Currently supported hides: hide, fadeOut, slideUp, remove.
+ * Currently supported hides: hide, fadeOut, slideUp, remove, toggle, fadeToggle, slideToggle,
+ *     css (display, visibility, opacity).
  * When a hide `h` is called, the event "hide.h" is triggered. For example, your object may receive
  * the event "hide.fadeOut", if a user has called .fadeOut() on it.
  * TODO: Animate css properties
  * TODO: Setting of the style attribute
- * TODO: Css changes: .css('display','none') and .css('visibility','hidden') and .css('opacity',0)
  * TODO: Class toggles: .addClass('hidden'), .toggleClass(...), etc.
  * TODO: Consider Mutation Observer https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
  */
@@ -34,7 +34,28 @@
             if(this.css('display') != "none")
                 this.trigger("hide."+event);
             
+            //Call old function
             old_function.apply(this, arguments);
         };
     });
+    
+    //jQuery set css properties
+    var old_function = $.fn.css;
+    
+    $.fn.css = function() {
+        //Case: display: none;
+        if(arguments[0] == "display" && arguments[1] == "none")
+            this.trigger("hide.css.display");
+        
+        //Case: visibility: hidden;
+        if(arguments[0] == "visibility" && arguments[1] == "hidden")
+            this.trigger("hide.css.visibility");
+            
+        //Case: opacity: 0;
+        if(arguments[0] == "opacity" && arguments[1] == 0)
+            this.trigger("hide.css.opacity");
+            
+        //Call old function
+        old_function.apply(this, arguments);
+    };
 })(jQuery);
