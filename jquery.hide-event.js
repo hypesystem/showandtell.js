e40e7df2344f3901fea8fe5b609dddb127ccfbb3
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-01-13 18:11:12 +0100

    Added support for toggle events

--- jquery.hide-event.js
+++ jquery.hide-event.js
@@ -3,7 +3,6 @@
  * Currently supported hides: hide, fadeOut, slideUp, remove.
  * When a hide `h` is called, the event "hide.h" is triggered. For example, your object may receive
  * the event "hide.fadeOut", if a user has called .fadeOut() on it.
- * TODO: Toggle functions: .toggle(), .fadeToggle(), slideToggle()
  * TODO: Animate css properties
  * TODO: Setting of the style attribute
  * TODO: Css changes: .css('display','none') and .css('visibility','hidden') and .css('opacity',0)
@@ -11,15 +10,31 @@
  * TODO: Consider Mutation Observer https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
  */
 (function ($) {
-  //Event on all different hides (by overwriting the jQuery functions).
-  $.each(['hide','fadeOut','slideUp','remove'], function (i, event) {
-    //Find usual function
-    var old_function = $.fn[event];
-    
-    //Overwrite with new function
-    $.fn[event] = function () {
-      this.trigger("hide."+event);
-      old_function.apply(this, arguments);
-    };
-  });
+    //jQuery hide functions
+    $.each(['hide','fadeOut','slideUp','remove'], function(i, event) {
+        var old_function = $.fn[event];
+        
+        //Overwrite with new function
+        $.fn[event] = function () {
+            //Trigger event
+            this.trigger("hide."+event);
+            
+            //Call old function
+            old_function.apply(this, arguments);
+        };
+    });
+      
+    //jQuery toggle functions
+    $.each(['toggle','fadeToggle','slideToggle'], function(i, event) {
+        var old_function = $.fn[event];
+        
+        //Overwrite with new function
+        $.fn[event] = function() {
+            //If event is currently shown it is being hidden
+            if(this.css('display') != "none")
+                this.trigger("hide."+event);
+            
+            old_function.apply(this, arguments);
+        };
+    });
 })(jQuery);
