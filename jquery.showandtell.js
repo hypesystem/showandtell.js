9279fbc04d8ae3ca5ea23865c2d839ea7f73a64d
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-02-02 20:30:24 +0100

    Check if lib has already been run Only run if this is first time.

--- jquery.showandtell.js
+++ jquery.showandtell.js
@@ -12,72 +12,81 @@
  *     is an alernative. (Project seems to be closed, maybe rewrite?)
  */
 (function ($) {
-    //jQuery hide functions
-    var old_hide = $.fn.hide;
-    
-    //Overwrite with new function
-    $.fn.hide = function() {
-        var was_hidden = $(this).is(":hidden");
-    
-        //Call old function
-        var result = old_hide.apply(this, arguments);
-    
-        //Trigger event
-        if(!was_hidden)
-            this.trigger("hide",{
-                type: "action"
-            });
-        
-        //Return
-        return result;
-    };
-    
-    var old_show = $.fn.show;
-    
-    $.fn.show = function() {
-        var was_hidden = $(this).is(":hidden");
-    
-        var result = old_show.apply(this, arguments);
-        
-        if(was_hidden)
-            this.trigger("show",{
-                type: "action"
-            });
-        
-        return result;
-    };
-    
-    //jQuery set css properties
-    var old_css = $.fn.css;
-    
-    $.fn.css = function() {
-        var was_hidden = $(this).is(":hidden");
-    
-        var result = old_css.apply(this, arguments);
-        
-        //Case: display: none;
-        if(!was_hidden && arguments[0] == "display" && arguments[1] == "none")
-            $(this).trigger("hide",{
-                type: "css"
-            });
-            
-        if(was_hidden && arguments[0] == "display" && arguments[1] != "none")
-            $(this).trigger("show",{
-                type: "css"
-            });
-        
-        return result;
-    };
-    
-    var old_remove = $.fn.remove;
-        
-    $.fn.remove = function() {
-        //Remove has to trigger event before removing. After removal, there is no element to
-        //  trigger the event on.
-        this.trigger("remove",{
-            type: "action"
-        });
 
-        return old_remove.apply(this, arguments);
-    };
+    //Only register if not already registered
+    if(!$.showandtell) {
+
+        //Indicate that showandtell has been registered.
+        $.showandtell = true;
+
+        //jQuery hide functions
+        var old_hide = $.fn.hide;
+
+        //Overwrite with new function
+        $.fn.hide = function() {
+            var was_hidden = $(this).is(":hidden");
+
+            //Call old function
+            var result = old_hide.apply(this, arguments);
+
+            //Trigger event
+            if(!was_hidden)
+                this.trigger("hide",{
+                    type: "action"
+                });
+
+            //Return
+            return result;
+        };
+
+        var old_show = $.fn.show;
+
+        $.fn.show = function() {
+            var was_hidden = $(this).is(":hidden");
+
+            var result = old_show.apply(this, arguments);
+
+            if(was_hidden)
+                this.trigger("show",{
+                    type: "action"
+                });
+
+            return result;
+        };
+
+        //jQuery set css properties
+        var old_css = $.fn.css;
+
+        $.fn.css = function() {
+            var was_hidden = $(this).is(":hidden");
+
+            var result = old_css.apply(this, arguments);
+
+            //Case: display: none;
+            if(!was_hidden && arguments[0] == "display" && arguments[1] == "none")
+                $(this).trigger("hide",{
+                    type: "css"
+                });
+
+            if(was_hidden && arguments[0] == "display" && arguments[1] != "none")
+                $(this).trigger("show",{
+                    type: "css"
+                });
+
+            return result;
+        };
+
+        var old_remove = $.fn.remove;
+
+        $.fn.remove = function() {
+            //Remove has to trigger event before removing. After removal, there is no element to
+            //  trigger the event on.
+            this.trigger("remove",{
+                type: "action"
+            });
+
+            return old_remove.apply(this, arguments);
+        };
+
+    }
 })(jQuery);
